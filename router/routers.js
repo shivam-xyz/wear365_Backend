ce34bafb3f0517682bc63c6d8edc682f8d43591a
shivam-xyz <75421877+shivam-xyz@users.noreply.github.com>
2023-02-21 23:22:37 +0530

    verify User Data Set In Response

--- router/routers.js
+++ router/routers.js
@@ -41,7 +41,7 @@ router.post('/api/registeruser', upload.single('myFile'), async (req, res) => {
 router.post('/api/verifyuser', async (req, res) => {
     try {
         console.log(req.body,41)
-        const isUserExist = await User.findOne({ mobile: req.body.mobile });
+        const isUserExist = await User.findOne({ mobile: req.body.mobile }).select({tokens:0});
         if (isUserExist) {
             const isPasswordMatched = await bcrypt.compare(req.body.password, isUserExist.password);
             if (isPasswordMatched) {
@@ -50,7 +50,7 @@ router.post('/api/verifyuser', async (req, res) => {
                 res.status(200).cookie('authToken', token, {
                     httpOnly: true,
                     expires: new Date(Date.now() + 600000)
-                }).send({ message: 'User Authenticated Successfully' })
+                }).send({ message: 'User Authenticated Successfully', response : isUserExist })
             }
             else {
                 res.status(400).send({ message: 'Incorrect Password' })
